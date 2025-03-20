07eec587bc31e804e2af05180987bd2064fa769c
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2025-03-19 23:16:00 -0300

    Css modules/global, Componente header e Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <h1>hello world</h1>
-      <Post
-        author="Vinicius Pacheco"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem maxime vero perspiciatis consectetur. Sint non sunt consequatur, necessitatibus exercitationem laborum itaque labore commodi possimus libero consectetur esse. Dolores, rem."
-      />
+      <Header />
 
-      <Post
-        author="Tiago alegre"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem maxime vero perspiciatis consectetur. Sint non sunt consequatur, necessitatibus exercitationem laborum itaque labore commodi possimus libero consectetur esse. Dolores, rem."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Vinicius Pacheco"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem maxime vero perspiciatis consectetur. Sint non sunt consequatur, necessitatibus exercitationem laborum itaque labore commodi possimus libero consectetur esse. Dolores, rem."
+          />
+
+          <Post
+            author="Tiago alegre"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem maxime vero perspiciatis consectetur. Sint non sunt consequatur, necessitatibus exercitationem laborum itaque labore commodi possimus libero consectetur esse. Dolores, rem."
+          />
+        </main>
+      </div>
     </>
   );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -4,6 +4,5 @@ export function Post(props) {
       <strong>{props.author}</strong>
       <p>{props.content}</p>
     </>
-    
   );
 }
